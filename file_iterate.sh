f5c607343783090a8ff9ad147919ce24217d81fe
Jayesh Wadile <jayeshwadile44@gmail.com>
2022-07-17 22:47:10 +0530

    test_change

--- file_iterate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-names="/home/jayesh/scripts/name.test"
-
-for name in $(cat $names)
-do
-	echo characters of hera pheri is $name
-done
--- user_input.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo what is your name?
-read name
-echo hi $name,welcome
